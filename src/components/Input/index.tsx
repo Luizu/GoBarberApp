e1f400039db4028ef01aa0f32eec1551e19ecd80
Luiz Castilho <luizucastilho@gmail.com>
2020-09-20 22:36:16 -0300

    Added validation Input feature using Yup

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,6 +3,8 @@ import React, {
   useImperativeHandle,
   useRef,
   forwardRef,
+  useState,
+  useCallback,
 } from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
@@ -32,6 +34,19 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
 
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value); // Thats the same is use if the check the value is empty or not
+  }, []);
+
   useImperativeHandle(ref, () => ({
     focus() {
       inputElementRef.current.focus();
@@ -55,9 +70,15 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused} isErrored={!!error}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
       <TextInput
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -6,11 +6,15 @@ import {
   KeyboardAvoidingView,
   Platform,
   TextInput,
+  Alert,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -19,6 +23,12 @@ import logoImg from '../../assets/logo.png';
 
 import { Container, BackToSignIn, BackToSignInText, Title } from './styles';
 
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
@@ -26,8 +36,41 @@ const SignUp: React.FC = () => {
   const emailInputRef = useRef<TextInput>(null);
   const passwordInputRef = useRef<TextInput>(null);
 
-  const handleSignUp = useCallback((data: object) => {
-    console.log(data);
+  const handleSignUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail valido'),
+        password: Yup.string().min(6, 'No minimo 6 digitos'),
+      });
+      await schema.validate(data, { abortEarly: false });
+
+      // await api.post('users', data);
+
+      Alert.alert(
+        'Cadastro realizado!',
+        'Você já pode fazer seu logon no GoBarber!',
+      );
+
+      // history.push('/');
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert(
+        'Erro no cadastro',
+        'Ocorreu um erro ao realizar o seu cadastro, tente novamente',
+      );
+    }
   }, []);
 
   return (
@@ -54,7 +97,7 @@ const SignUp: React.FC = () => {
                 icon="user"
                 placeholder="Nome"
                 returnKeyType="next"
-                onSubmitEditing={() => emailInputRef.current.focus()}
+                onSubmitEditing={() => emailInputRef.current?.focus()}
               />
 
               <Input
@@ -66,7 +109,7 @@ const SignUp: React.FC = () => {
                 icon="mail"
                 placeholder="E-mail"
                 returnKeyType="next"
-                onSubmitEditing={() => passwordInputRef.current.focus()}
+                onSubmitEditing={() => passwordInputRef.current?.focus()}
               />
 
               <Input
