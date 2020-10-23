e8e71e6abde7e8bf97137d3a6e05c899733b32f8
luizu <luizucastilho@gmail.com>
2020-10-22 23:13:47 -0300

    FIxed semicollon missing

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -10,7 +10,7 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   padding: 24px;
-  padding-top: ${getStatusBarHeight() + 24}px
+  padding-top: ${getStatusBarHeight() + 24}px;
   background: #282623;
 
   flex-direction: row;
